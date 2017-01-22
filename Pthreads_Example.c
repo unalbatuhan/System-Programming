65903f499a652a3d75f37e4ab9629aa517033873
Batuhan UNAL <unal.batuhan@outlook.com>
2017-01-23 00:54:59 +0200

    Update Pthreads_Example.c

--- Pthreads_Example.c
+++ Pthreads_Example.c
@@ -14,3 +14,11 @@ if (atoi(argv[1] < 0)){
 fprintf(stderr, "%d must be > = 0\n", atoi(argv[1]));
 return -1;
 }
+  /* get the default attributes */
+  pthread_attr_t_init (&attr);
+  /* create thread */
+  pthread_create(&tid, &attr, runner, args[1]);
+  /* wait for the thread to exit */
+  pthread_join(tid, NULL);
+  printf("sum = %d\n", sum);
+  
